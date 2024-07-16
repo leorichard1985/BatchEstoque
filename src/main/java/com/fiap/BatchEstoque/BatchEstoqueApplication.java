bb630d14b97c31b94b1ecbeecb71497e9bb5b654
Leandro Conceição <leandro.vs2010@hotmail.com>
2024-07-15 21:30:40 -0300

    Estrutura do projeto

---
+++ src/main/java/com/fiap/BatchEstoque/BatchEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.BatchEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/BatchEstoque/BatchEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.BatchEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
